27fb2bc4f0c25fdaf1d8a3de4148412ac6c6cde7
brett f <1brettf@gmail.com>
2020-02-29 21:15:41 -0800

    Hook up .env and now secrets

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Airtable from 'airtable';
 
 const base = new Airtable({
   apiKey: process.env.REACT_APP_AIRTABLE_API_KEY
-}).base(process.env.REACT_APP_AIRTABLE_BASE_ID);
+}).base('appUcjUVkCCkgJJcl');
 const inventory = base('Inventory');
 
 function App() {
