f99cd993a0bdc7b7a50122720781a3fe34d1ecd0
brett f <1brettf@gmail.com>
2020-03-02 17:09:56 -0800

    Filter by foreman

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import Airtable from 'airtable';
 
+const foreman = 'Branimir Jesic';
+
 const base = new Airtable({
   apiKey: process.env.REACT_APP_AIRTABLE_API_KEY
 }).base('appUcjUVkCCkgJJcl');
@@ -13,37 +15,40 @@ function App() {
 
   useEffect(() => {
     inventory
-      .select({ view: 'DO NOT TOUCH - API' })
+      .select({
+        view: 'DO NOT TOUCH - API',
+        filterByFormula: `({Assigned To} = '${foreman}')`
+      })
       .all()
-      .then(records => {
-        console.log('Records', records);
-        setData(records);
+      .then(recs => {
+        console.log('Records', recs);
+        setData([...recs]);
       });
   }, []);
 
   return (
-    <main>
-      <iframe
+    <>
+      <h1>{foreman}</h1>
+      <main>
+        {/* <iframe
         className="airtable-embed"
         title="Specialty Tools Calendar"
         src="https://airtable.com/embed/shrgP7DXhPWLAqpf1?backgroundColor=gray"
         frameBorder="0"
         width="100%"
         height="533"
-      ></iframe>
-      {data.map(record => (
-        <div className="record" key={record.id}>
-          <p>{record.fields['Tool ID']}</p>
-          <p>{record.fields['Type']}</p>
-          <p>{record.fields['Serial']}</p>
-          <p>{record.fields['Description']}</p>
-          <p>{record.fields['Model']}</p>
-          <p>{record.fields['Purchase Date']}</p>
-          <p>{record.fields['Status']}</p>
-          <p>{record.fields['Assigned To']}</p>
-        </div>
-      ))}
-    </main>
+      /> */}
+        {data.map(rec => (
+          <div className="record" key={rec.id}>
+            <p>{rec.fields['Type']}</p>
+            <p>{rec.fields['Tool ID']}</p>
+            <p>{rec.fields['Description']}</p>
+            <p>{rec.fields['Status']}</p>
+            <p>{rec.fields['Assigned To']}</p>
+          </div>
+        ))}
+      </main>
+    </>
   );
 }
 
