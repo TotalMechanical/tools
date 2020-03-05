9d1868372bbe89f1b86996201f6aff15605d3ccc
brett f <1brettf@gmail.com>
2020-03-05 15:58:30 -0800

    Show specialty calendar embed

--- src/components/SpecialtyCalendar.js
+++ src/components/SpecialtyCalendar.js
@@ -13,15 +13,15 @@ export default function SpecialtyCalendar() {
       {showCal && (
         <>
           <p>Cal</p>
-          {/* <iframe
+          <iframe
             title="Specialty Calendar"
             className="airtable-embed"
-            src="https://cors-anywhere.herokuapp.com/https://airtable.com/embed/shrPxBnmDnm2lnEsh?backgroundColor=white"
+            src="https://airtable.com/embed/shrPxBnmDnm2lnEsh?backgroundColor=white"
             frameBorder="0"
             width="100%"
             height="250"
             style={{ backgroundColor: 'transparent', border: '1px solid #ccc' }}
-          /> */}
+          />
         </>
       )}
     </div>
